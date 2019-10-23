43554ce1adb62cea5e3f89eac2dab85500755d12
sal-pal-05 <C00137488@louisiana.edu>
2019-10-23 09:43:42 -0500

    finished HW5

--- HW5 adply dataset loading.R
+++ HW5 adply dataset loading.R
@@ -11,8 +11,17 @@ data1=adply(data_set, .margins = 1, function(file){
   ##read the data
   d= read_csv(file)
   
+##removing 1st "QF column"  
+  d$QF=NULL
+
+  
+  ##created new column"test"
+  
+  d$test=seq(0,(nrow(d)-1)*5,5)
+    
   return(d)
 }, .inform = T)
+
 
 
 save(data1, file="Datasets For Class")
